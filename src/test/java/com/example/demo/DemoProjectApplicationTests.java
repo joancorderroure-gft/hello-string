ab257f154ab6981d8c29842fbee9ff874ebeb948
joancorderroure-gft <JNOM@gft.com>
2022-01-28 13:40:16 +0100

    Added more tests

--- src/test/java/com/example/demo/DemoProjectApplicationTests.java
+++ src/test/java/com/example/demo/DemoProjectApplicationTests.java
@@ -1,6 +1,8 @@
 package com.example.demo;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -35,12 +37,52 @@ class DemoProjectApplicationTests {
 		assertThat(restTemplate.getForObject("/hello", String.class))
 				.isEqualTo("Hello World!");
 	}
+
+	@Test
+	void helloNames(@Autowired TestRestTemplate restTemplate) {
+		String[] arr = {"Javier", "Rodriguez", "Javier+Arturo"};
+		for (String name: arr) {
+			assertThat(restTemplate.getForObject("/hello?name="+name, String.class))
+					.isEqualTo("Hello "+name+"!");
+		}
+	}
+
+	@Autowired TestRestTemplate restTemplate;
+	@ParameterizedTest
+	@ValueSource(strings = {"Javier", "Rodriguez", "Arturo"})
+	void helloParamNames(String name) {
+		assertThat(restTemplate.getForObject("/hello?name="+name, String.class))
+				.isEqualTo("Hello "+name+"!");
+	}
+
 	@Test
 	void canAdd(@Autowired TestRestTemplate restTemplate) {
 		assertThat(restTemplate.getForObject("/add?a=1 &b=2", String.class))
 				.isEqualTo("3");
 	}
+	@Test
+	void canAddNegative(@Autowired TestRestTemplate restTemplate) {
+		assertThat(restTemplate.getForObject("/add?a=1 &b=-2", String.class))
+				.isEqualTo("-1");
+	}
+
+	@Test
+	void canAddNullA(@Autowired TestRestTemplate restTemplate) {
+		assertThat(restTemplate.getForObject("/add?a=&b=1", String.class))
+				.isEqualTo("1");
+	}
+	@Test
+	void canAddNullB(@Autowired TestRestTemplate restTemplate) {
+		assertThat(restTemplate.getForObject("/add?a=1&b=", String.class))
+				.isEqualTo("1");
+	}
+
+	void canAddFractions(@Autowired TestRestTemplate restTemplate) {
+		assertThat(restTemplate.getForObject("/add?a=1.5&b=2", String.class))
+				.isEqualTo("3.5");
+	}
 
 }
+
 
 
